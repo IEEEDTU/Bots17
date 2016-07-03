ffa24c956c64c39c8b34b6b21a397842e85f0f49
Himanshu Chauhan <hchauhan1404@outlook.com>
2016-07-03 12:25:27 +0530

    Added TODOs.

---
+++ main.py
@@ -0,0 +1,20 @@
+"""
+Main file of the project. All functions are to be defined here.
+"""
+
+# Board (11 * 11 matrix) initialized with 'U'
+board = [['U' for i in range(0, 11)] for j in range(0, 11)]
+
+# This function checks whether a new piece can be placed or not.
+# TODO: is_valid_move()
+
+# This functions first checks for validity of new move and then
+# update Board with new move.
+# TODO: move()
+
+# This function checks if any player is winning or not.
+# TODO: check_winning()
+
+# This function drives the program and plays the game.
+# TODO: main function()
+
