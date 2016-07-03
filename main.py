71d95f85464a5616ec50a5e16e112b7354994fb7
Shubhi Sareen <shubhisareen@gmail.com>
2016-07-03 13:58:18 +0530

    Added functions is_valid_move() and move()

--- main.py
+++ main.py
@@ -6,11 +6,21 @@ Main file of the project. All functions are to be defined here.
 board = [['U' for i in range(0, 11)] for j in range(0, 11)]
 
 # This function checks whether a new piece can be placed or not.
-# TODO: is_valid_move()
+def is_valid_move( x, y ):
+	if(x<11 and y<11 and board[x][y]=='U'):
+		return True;
+	else:
+		return False;
 
 # This functions first checks for validity of new move and then
 # update Board with new move.
-# TODO: move()
+def move(x, y, color):
+	if(is_valid_move(x,y)):
+		global board
+		board[x][y] = color
+		return True;
+	else:
+		return False;
 
 # This function checks if any player is winning or not.
 # TODO: check_winning()
