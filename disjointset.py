eefccf349c8be264914f750b39be680edc2d67cc
Nikhil Pandey <menikhilpandey@gmail.com>
2016-07-05 19:28:06 +0530

    Update disjointset.py
    FIX Required: root() function

--- disjointset.py
+++ disjointset.py
@@ -4,25 +4,28 @@ class DisjointSet:
         self.Size = []
 
     def initialize(self):
-        for i in range(0, 169):
+        for i in range(169):
             self.Arr.append(i)
             self.Size.append(1)
 
-        for i in range(1, 13):
-            self.Arr[i], self.Arr[117 + i] = 1, 117  # Top and Bottom virtual cells.
-            self.Arr[13 * i], self.Arr[13 * i + 12] = 13, 25  # Left and Right virtual cells.
+        for i in range(1, 12):
+            self.Arr[i], self.Arr[156 + i] = 1, 2  # Top and Bottom virtual cells.
+            self.Arr[13 * i], self.Arr[13 * i + 12] = 3, 4  # Left and Right virtual cells.
 
-    def calc_id(self,x, y):
-        return (x + 1) * 13 + (y + 1)
+    def calc_id(self, x, y):
+        return x * 13 + y
 
     def root(self, i):
-        if self.Arr[i] != i:
-            self.Arr[i] = self.root(self.Arr[i])
-        return self.Arr[i]
+        # TODO: define correctly
+        pass
 
     def merge(self, a, b):
         root_a, root_b = self.root(a), self.root(b)
-        if root_a != root_b:
+        if root_a in [1, 2, 3, 4]:
+            self.Arr[root_b], self.Size[root_a] = self.Arr[root_a], self.Size[root_a] + self.Size[root_b]
+        elif root_b in [1, 2, 3, 4]:
+            self.Arr[root_a], self.Size[root_b] = self.Arr[root_b], self.Size[root_b] + self.Size[root_a]
+        elif root_a != root_b:
             if self.Size[root_a] < self.Size[root_b]:
                 self.Arr[root_a], self.Size[root_b] = self.Arr[root_b], self.Size[root_b] + self.Size[root_a]
             else:
@@ -30,4 +33,3 @@ class DisjointSet:
 
     def find_set(self, a, b):
         return self.root(a) == self.root(b)
-
