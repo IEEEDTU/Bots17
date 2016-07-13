d96b0c3149a97835a0a263d7e280f41da0bbc929
Himanshu Chauhan <hchauhan1404@outlook.com>
2016-07-13 17:23:55 +0530

    Added checker files as per request.

---
+++ checker1.py
@@ -0,0 +1,40 @@
+import timeit
+from main import move
+
+
+def func():
+    # Input #1
+    move(5, 5, 'R')
+    move(9, 1, 'B')
+    move(6, 3, 'R')
+    move(8, 1, 'B')
+    move(7, 1, 'R')
+    move(7, 2, 'B')
+    move(6, 2, 'R')
+    move(7, 0, 'B')
+    move(8, 0, 'R')
+    move(6, 1, 'B')
+    move(4, 7, 'R')
+    move(5, 2, 'B')
+    move(4, 8, 'R')
+    move(4, 3, 'B')
+    move(10, 3, 'R')
+    move(3, 4, 'B')
+    move(6, 4, 'R')
+    move(2, 5, 'B')
+    move(5, 6, 'R')
+    move(1, 6, 'B')
+    move(10, 0, 'R')
+    move(0, 7, 'B')
+    move(10, 4, 'R')
+    move(10, 1, 'B')
+    move(5, 8, 'R')
+    move(4, 9, 'B')
+    move(10, 2, 'R')
+    move(3, 9, 'B')
+    move(5, 9, 'R')
+    move(2, 9, 'B')
+    move(5, 10, 'R')
+
+
+print(timeit.timeit(func, number=1000))
